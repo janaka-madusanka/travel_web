dd4e9fa3c511140052c125a167154c7f74fa58de
Rashen Themiya <rashenrashen4@gmail.com>
2025-12-06 02:38:20 -0800

    dockerfile

--- app/admin/dashboard/layout.tsx
+++ app/admin/dashboard/layout.tsx
@@ -10,8 +10,8 @@ interface AdminLayoutProps {
 export default function AdminLayout({ children }: AdminLayoutProps) {
   return (
     <div className="flex min-h-screen">
-      <Sidebar />
-      <main className="flex-1 p-6 bg-gray-100">{children}</main>
+      <Sidebar />                     {/* Sidebar visible on all admin pages */}
+      <main className="flex-1 p-6 bg-gray-100">{children}</main>  {/* Page content */}
     </div>
   );
 }
