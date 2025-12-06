ff42f5c490c0123732e459ded01c8845d59dbc5e
Rashen Themiya <rashenrashen4@gmail.com>
2025-12-05 23:29:22 -0800

    fix

---
+++ app/admin/dashboard/layout.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { ReactNode } from "react";
+import Sidebar from "./Sidebar";
+
+interface AdminLayoutProps {
+  children: ReactNode;
+}
+
+export default function AdminLayout({ children }: AdminLayoutProps) {
+  return (
+    <div className="flex min-h-screen">
+      <Sidebar />
+      <main className="flex-1 p-6 bg-gray-100">{children}</main>
+    </div>
+  );
+}
